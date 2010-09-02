ee7cec00bec0e38d323eb792cffd4be3b830199c
Boris Gordon <boris@boztek.net>
2010-09-03 00:21:24 +0200

    auto-yes when rebuilding

--- rebuild.sh
+++ rebuild.sh
@@ -5,9 +5,7 @@ DRUSH='/usr/local/bin/drush'
 PROJECT='newsite'
 
 if [ -f "$PROJECT.make" ]; then
-  echo "inside profile so can rebuild in place"
-  # echo $PROXY
-  http_proxy="http://localhost:3128/" $DRUSH make --working-copy --no-core --contrib-destination=. "$PROJECT.make"
+  http_proxy="http://localhost:3128/" $DRUSH make --working-copy --no-core --contrib-destination=. "$PROJECT.make" --yes
 else
   echo "no inside profile - not sure what to do"
 fi
