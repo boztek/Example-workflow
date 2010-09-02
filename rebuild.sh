32b0629b529ef44615d84a71baec6b92ff4027a8
Boris Gordon <boris@boztek.net>
2010-09-02 18:45:10 +0200

    starting to sketch rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,8 @@ PROJECT='newsite'
 
 if [ -f "$PROJECT.make" ]; then
   echo "inside profile so can rebuild in place"
-  echo $PROXY
+  # echo $PROXY
+  http_proxy="http://localhost:3128/" $DRUSH make --working-copy --no-core --contrib-destination=. "$PROJECT.make"
 else
   echo "no inside profile - not sure what to do"
 fi
