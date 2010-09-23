e8af4d5da838c0620ff87406366a06ead6cddf5c
Boris Gordon <boris@boztek.net>
2010-09-24 00:02:06 +0200

    start to generalise reinstall script

--- reinstall.sh
+++ reinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+PROFILE=newsite
+WEBROOT=/Users/boris/Sites
+SITE=newsite.com
+INSTANCE=newsite.local
+
 cd ~/Sites/newsite.com/htdocs/sites/newsite.local
+cd $WEBROOT/$SITE/htdocs/sites/$INSTANCE
 drush sql-dump > backup.sql
 drush sql-dump --add-drop-table --no-data | grep ^DROP | drush sqlc
-open "http://newsite.local/install.php?profile=newsite"
+open "http://$INSTANCE/install.php?profile=$PROFILE"
