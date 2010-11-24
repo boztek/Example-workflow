09aee39321ee000e6508a178e80f928a88e3efc5
Boris Gordon <boris@boztek.net>
2010-11-24 02:35:31 +0100

    generalised resinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 PROFILE=newsite
-WEBROOT=/Users/boris/Sites
-SITE=newsite.com
-INSTANCE=newsite.local
+WEBROOT="$HOME/Sites"
+PROJECT=workflow.com
+INSTANCE=$1
 
-cd ~/Sites/newsite.com/htdocs/sites/newsite.local
-cd $WEBROOT/$SITE/htdocs/sites/$INSTANCE
-drush sql-dump > backup.sql
+if [ "$#" -lt 1 ]; then
+  echo "need a site instance"
+  exit
+fi
+
+cd build/current/sites/$INSTANCE
 drush sql-dump --add-drop-table --no-data | grep ^DROP | drush sqlc
 open "http://$INSTANCE/install.php?profile=$PROFILE"
